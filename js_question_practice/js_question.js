c8d440cc482a2eb6fa3bb904adb4337aabf25c8a
vikasverma-here <vikasvetma995619@gmail.com>
2024-10-04 14:04:35 +0530

    just commited

--- js_question_practice/js_question.js
+++ js_question_practice/js_question.js
@@ -1,1 +1,28 @@
-alert("hellow my boy")
+function print() {
+    console.log(arguments[0])
+  }
+  print('helllow',false, 400)
+
+//   const obj = {
+//     name: 'deeecode',
+//     age: 200,
+//     print: function() {
+//       console.log(this)
+//     }
+//   }
+  
+//   obj.print()
+
+  const obj = {
+  name: 'deeecode',
+  age: 200,
+  print: function() {
+    function print2() {
+      console.log(this)
+    }
+
+    print2()
+  }
+}
+
+obj.print()
