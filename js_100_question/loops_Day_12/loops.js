1caeedb0d0fde38f6023f060363509e86f5f0ec9
vikasverma-here <vikasverma995619@gmail.com>
2024-10-06 10:02:51 +0530

    loops is going on

---
+++ js_100_question/loops_Day_12/loops.js
@@ -0,0 +1,33 @@
+// !! 23- Accept an integer and Print hello world n times =====================================================>
+    
+    // let number  = +prompt("enter a number to print hellow number of times ")
+    // for(let i=1; i<=number; i++ ){
+    //     console.log("hellow")
+    // }
+
+
+// !!! Print natural number up to n.  ===============================================================================>
+    // let n = +prompt("enter number to all natural number between one to n ")
+    // for(let i=1; i<=n; i++){
+    //     console.log(i)
+    // }
+
+// !! 25- Reverse for loop. Print n to 1.========================================================>
+    // let n = +prompt("enter the number to print reverse of the number between n to 1 ")
+    // for(let i=n ; i>=1; i--){
+    //     console.log(i)
+    // }
+
+// !! 26- Take a number as input and print its table ==================================================>
+    // let number = +prompt("enter the number for table ")
+    // for(let i=1; i<=10; i++){
+    //     console.log(number ,'* ' ,i ,'= ',number*i )
+    // }
+
+// !! 27- Sum up to n terms. =====>
+    // let n = +prompt("enter the number for sum up to n terms ")
+    // let sum = 0;
+    // for(let i=1; i<=n; i++){
+    //     sum = sum+i;
+    // }
+    // console.log(sum)
