546c37a0a835f6ed280ddd49729dd28883c80e1b
vikasverma-here <vikasverma995619@gmail.com>
2024-10-14 20:26:00 +0530

    learned about error handling

--- js_100_question/strings/string.js
+++ js_100_question/strings/string.js
@@ -15,3 +15,54 @@
 //     console.log("not this string is strat with this substring ")
 // }
 
+
+
+// !!!! ERROR HANDLING ================================================================================>
+
+    // ??try catch block is used to handle synchronous ( code which runs line by line ) code errors
+
+    // try{
+    //     let a = 23;
+    //     console.log(a+b)
+    // }catch(error){
+    //     // error ek object hai jo ki name and message deta hai seprately 
+    //     console.log(error)
+    //     console.log(error.name)
+    //     console.log(error.message)
+    // }  
+//    console.log("hellow")
+
+// !!! finaly in error handling 
+
+// try{
+//     let a=1;
+//     console.log(a+b)
+// }catch(error){
+//     console.log(error)
+// }finally{
+//     console.log("ye code to always chalega hi chalega kuch bhi ho jaye ")
+// }
+// console.log("hellow world ")
+
+
+// !!! HOW TO GENRATE CUSTOM ERROR 
+
+// try{
+//     let a=12;
+//     let b = 2;
+//     if(a%b==0){
+//         throw new TypeError("Even is not allowes")
+//     }else{
+//         throw new Error("odd is not allowed ")
+//     }
+// }catch (error){
+// if(error.name == "TypeError"){
+//     console.log("even value is not allowed ")
+//     console.log(error.name)
+// }else if(error.name == "Error"){
+//     console.log(error.message)
+//     console.log("odd value is not allowed ")
+// }else{
+//     console.log("something went wrong ")
+// }
+// }
