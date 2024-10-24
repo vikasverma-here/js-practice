fd0cd2b29f235dcf45b22d41472613b5ce279e90
vikasverma-here <vikasverma995619@gmail.com>
2024-10-24 19:28:48 +0530

    some question done

--- js_100_question/day 22-29/script.js
+++ js_100_question/day 22-29/script.js
@@ -1,4 +1,4 @@
-// !!!!!! 54- Accept size n from user and create a n size array then take n inputs into the and finally 
+// !!!!!! 54- Accept size n from user and create a n size array then take n inputs into the and finally
 //  !!     Print the sum & Avg of all elements.===========================================================================>
 
 //     let size  = parseInt(prompt("enter the size of array"))
@@ -13,7 +13,7 @@
 //     sum = sum+arrSize[i];
 
 //     }
-//  console.log ('sum of array element is ' , sum )   
+//  console.log ('sum of array element is ' , sum )
 
 // if(size>0){
 //     let average=sum/size;
@@ -22,7 +22,6 @@
 // else{
 //     console.log("size of array is less than zero")
 // }
-
 
 //  !!  55- Find the greatest element.
 //  !!    {2, 96, 69, 77, 145, 20} = Max element = 145 found at 4 index
@@ -36,17 +35,16 @@
 // }
 // console.log(largest,arr[largest])
 
-// !! 56- Find the second greatest element 
+// !! 56- Find the second greatest element
 //  !!     {2, 96, 69, 77, 145, 20} = Second greatest element = 96
-// let arr =  [2, 96, 69, 77, 145, 20] 
+// let arr =  [2, 96, 69, 77, 145, 20]
 // let first = 0;
 // let second = 0;
 // for(let i=0;i<arr.length;i++){
 //     if(arr[i]>first){
-       
+
 //         second = first;
 //         first=arr[i]
-        
 
 //     }else if (arr[i]>second && arr[i] != first ){
 //        second = arr[i]
@@ -55,11 +53,9 @@
 // console.log(first)
 // console.log(second)
 
-
 // !!!!57- Check if array is sorted in increasing order or not.
 //        Ex 1 - { 1, 5, 8,  9, 10, 15 } - OP = "YES"
 //        Ex 2 - { 1, 8, 6, 9, 10, 15 } - OP = "NO"
-
 
 // let arr = [1,2,3,4,5,9,7,8,9]
 // let arrIsSorted = true;
@@ -71,7 +67,7 @@
 // }
 // console.log(arrIsSorted)
 
-// !! 58- Take n integer inputs from user and store them in an array. 
+// !! 58- Take n integer inputs from user and store them in an array.
 //    !     Now, copy all the elements in an another array but in reverse order and print it.
 
 // let size = +prompt("enter the size of and array ")
@@ -86,16 +82,13 @@
 
 // !!59- Array left Rotation by 1
 
-
 // let arr = [1,2,3,4,4,56,7,7,12]
 // // let temp=arr[0]
 
 // for (let i = 0; i < arr.length-1; i++) {
-//    arr[i]=arr[i+1]    
+//    arr[i]=arr[i+1]
 // }
 // console.log (arr)
-
-
 
 // !!60- Array left rotation by K elements
 // let k = +prompt("enter the number of rotation ")
@@ -111,7 +104,6 @@
 // console.log(arr)
 // console.log(rotation)
 
-
 // !!61- Array Reverse Without Using Extra space
 
 // let arr = [3,4,5,6,7,8,86,5,44,33]
@@ -126,7 +118,6 @@
 //    right--;
 // }
 // console.log(arr)
-
 
 // !!!62- Linear Search an array - If element found print the index else -1
 
@@ -151,11 +142,11 @@
 //    let arr = [24,23,446,6,767,87,66,5,453,43]
 //  arr.sort((a,b)=> a-b)
 // console.log(arr)
-// let start=0; 
+// let start=0;
 // let end = arr.length-1;
 
 //    while(start<end ){
-//       let mid = Math.floor((start+end)/2 ) 
+//       let mid = Math.floor((start+end)/2 )
 //       if(arr[mid]==findElement){
 //          return mid;
 //       }else if(arr[mid]>findElement){
@@ -169,9 +160,101 @@
 
 // console.log(BinarySearch(findElement))
 
-
 // !!!! Bubble sort =========================================================>
-// let arr = [87, 35, 12, 59, 67, 99, 21, 45, 76, 11]
-// for(let i =0; i<=arr.length; i++){
-//     for(let j=0; )
+// let arr = [87, 35, 12, 59, 67, 99, 21, 45, 76, 11];
+
+// for (let i = 0; i < arr.length - 1; i++) {
+
+//   for (let j = i+1; j < arr.length; j++) {
+//     if (arr[i] > arr[j]) {
+     
+//       let temp = arr[i];
+//       arr[i] = arr[j];
+//       arr[j] = temp;
+//     }
+//   }
+//   ;
 // }
+
+// console.log(arr);
+
+
+// !!!!!65- Move all the negative elements on left side and positive elements on right side O(n).
+//  let arr = [12, -7, 5, -3, 9, -1, -8, 6, -4, 10, -11];
+//  let start = 0;
+//  let end = arr.length-1;
+//  while(start<=end){
+//     if(arr[start]<0){
+//         start++;
+//     }
+//     else if(arr[end]>0){
+//         end--;
+//     }
+//     else{
+//       [arr[start],arr[end]]=[arr[end],arr[start]]
+//         start++;
+//         end--;
+//     }
+//  }
+// console.log(arr)
+
+// !!!67- Strong number using methods
+
+// let number = 40585 ;
+
+
+// function StrongNumber (number ){
+//     let sum =0;
+// let original = number ;
+//     while(number>0){
+//         digit = number%10;
+//         console.log(digit)
+//         let fact =1;
+//         for(let i=1;i<=digit;i++){
+//           fact=fact*i;
+//         }
+//         sum = sum+fact;
+//         number = Math.floor(number/10) 
+//     }
+//     if(sum===original){
+//         console.log("strong number ")
+//     }else{
+//         console.log("not strong number ")
+//     }
+// }
+// StrongNumber(number)
+
+
+// !!!71- Accept a string from user and print its each character on a new line
+// let str = prompt("enter the string ")
+//  str.split('').forEach((e)=>{
+//     console.log(e)
+// })
+
+
+// !!!!72- Accept a string and print it in reverse order
+// let str = prompt("enter the string ")
+// let arr = str.split("").reverse("").join('')
+// console.log(arr)
+
+
+// !!! 73- Check if the string is Pallindromic or not
+
+// let str = prompt("enter the string ")
+// let arr = str.split("").reverse("").join('')
+// console.log(arr)
+// if(str===arr){
+//     console.log( str ,"Plaindrome",arr)
+// }else{
+//     console.log(str,"not a palindrome",arr)
+// }
+
+
+
+// !!!75- Count vowels and consonents in a string
+
+let str =prompt("enter string ")
+let newarr = str.split("")
+newarr.forEach((e)=>{
+    if(e==a)
+})
